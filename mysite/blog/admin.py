da38ac205df2a928de92edd02d7db6e74c1a7155
warui <waruingugientp@gmail.com>
2019-09-24 04:56:14 +0300

    Initial Commit

---
+++ mysite/blog/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
